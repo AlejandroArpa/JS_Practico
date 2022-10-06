c0b91f71001d8ecb1b24f77a7831076b47fa28f6
Alejandro <alejorp195@hotmail.com>
2022-10-06 16:16:30 -0500

    Menu movil agregado

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopmenu =document.querySelector('.desktop-menu');
+const burguerMenu=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+burguerMenu.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    desktopmenu.classList.toggle('inactive')
+    desktopmenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
