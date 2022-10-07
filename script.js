1935e8146f593b8c77d943544023916379ac671c
Alejandro Ramírez P <alejorp195@hotmail.com>
2022-10-06 21:40:39 -0500

    Desplegar y ocultar menus

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ const menuEmail =document.querySelector('.navbar-email');
 const desktopmenu =document.querySelector('.desktop-menu');
 const burguerMenu=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
+const carritoicon=document.querySelector('.navbar-shopping-cart');
+const aside=document.querySelector('.product-detail');
+let elementos=[desktopmenu,aside,mobileMenu];
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burguerMenu.addEventListener('click',toggleMobileMenu);
+carritoicon.addEventListener('click',toggleCarritoAside);
+
+function setInit(element){
+    for (let i=0; i<elementos.length;i++){
+        if(elementos[i]==element){
+            elementos[i].classList.toggle('inactive');
+        }
+        else{
+            elementos[i].classList.add('inactive');
+        }
+    }
+}
 
 function toggleDesktopMenu(){
-    desktopmenu.classList.toggle('inactive');
+    setInit(desktopmenu);   
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    setInit(mobileMenu); 
+}
+
+function toggleCarritoAside(){   
+    setInit(aside); 
 }
